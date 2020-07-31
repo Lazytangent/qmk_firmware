d3c0238de3e8dcce370a4eb54d5c02ce6a58373d
Peter <pm123633@gmail.com>
2020-07-31 15:11:16 -0500

    Tested for enum support in personal keymaps

--- keyboards/cannonkeys/ortho48/keymaps/lazytangent/keymap.c
+++ keyboards/cannonkeys/ortho48/keymaps/lazytangent/keymap.c
@@ -15,26 +15,20 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
-#include "keyDefinitions.h"
 
-
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
 #define _BASE 0
-#define _RAISE 1
-#define _LOWER 2
+#define _LOWER 1
+#define _RAISE 2
 #define _NUM 3
 #define _CHEESE 4
 
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  NUM,
-  CHEESE
-};
+/* enum layers {                Unable to use enum for layers on cannonkeys/ortho48 because of size restrictions
+  _BASE = SAFE_RANGE,
+  _LOWER,
+  _RAISE,
+  _NUM,
+  _CHEESE
+}; */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -46,14 +40,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Num  | Ctrl | Alt  | GUI  |Lower |Space |Sh_Spc|Raise | Left | Down |  Up  |Right |
+ * | Num  | Ctrl | Alt  | GUI  |Lower |Space |Space |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_BASE] = LAYOUT_ortho_4x12(
     KC_ESC,   KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,   KC_Y,    KC_U,       KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,       KC_G,   KC_H,    KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,   KC_N,    KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    MO(_NUM), KC_LCTL, KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, RS(SPC), MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    MO(_NUM), KC_LCTL, KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_SPC, MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
@@ -89,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_CAPS, KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    RGB_TOG, RGB_MOD, BL_INC,  BL_DEC,  _______, _______, _______, _______, _______, KC_BRID, KC_BRIU, MO(_CHEESE)
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BRID, KC_BRIU, MO(_CHEESE)
 ),
 
 /* Num -- Numpad Layer
@@ -104,9 +98,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_NUM] = LAYOUT_ortho_4x12(
-    _______, _______, _______, _______, _______, _______, KP_7,    KP_8,    KP_9,    KSSH,    KMIN,    KC_BSPC,
-    _______, _______, _______, _______, _______, _______, KP_4,    KP_5,    KP_6,    STAR,    PLUS,    _______,
-    _______, _______, _______, _______, _______, KP_0,    KP_1,    KP_2,    KP_3,    KDOT,    KEQL,    KNTR,
+    _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, KC_PMNS, KC_BSPC,
+    _______, _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PAST, KC_PPLS, _______,
+    _______, _______, _______, _______, _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PDOT, KC_PEQL, KC_PENT,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
@@ -123,8 +117,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_CHEESE] = LAYOUT_ortho_4x12(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, F_13,    F_14,    F_15,    F_16,    F_17,    F_18,    _______, _______, _______, _______, _______,
-    _______, F_19,    F_20,    F_21,    F_22,    F_23,    F_24,    _______, _______, _______, _______, _______,
+    _______, KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  _______, _______, _______, _______, _______,
+    _______, KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 };
