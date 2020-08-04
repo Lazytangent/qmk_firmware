cba34ecad663a7669fde0f99211791669cda671b
Peter <pm123633@gmail.com>
2020-08-04 17:19:35 -0500

    lily58 oleds

--- keyboards/lily58/keymaps/lazytangent/keyDefinitions.h
+++ keyboards/lily58/keymaps/lazytangent/keyDefinitions.h
@@ -1,4 +1,11 @@
-enum greek_keycodes {
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+#define _ADJUST 3
+#define _LIFT 4
+#define _GREEK 5
+
+enum custom_keycodes {
     ALPHA = SAFE_RANGE,
     BETA,
     GAMMA,
@@ -22,11 +29,76 @@ enum greek_keycodes {
     PHI,
     CHI,
     PSI,
-    OMEGA
+    OMEGA,
+    QWERTY,
+    LOWER,
+    RAISE,
+    ADJUST,
+    LIFT,
+    GREEK
 };
 
+void set_keylog(uint16_t keycode, keyrecord_t *record);
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        #ifdef OLED_DRIVER_ENABLE
+            set_keylog(keycode, record);
+        #endif
+        // set_timelog();
+    }
+
     switch (keycode) {
+        case QWERTY:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_QWERTY);
+            }
+            return false;
+            break;
+        case LOWER:
+            if (record->event.pressed) {
+                layer_on(_LOWER);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_LOWER);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            }
+            return false;
+            break;
+        case RAISE:
+            if (record->event.pressed) {
+                layer_on(_RAISE);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_RAISE);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            }
+            return false;
+            break;
+        case ADJUST:
+            if (record->event.pressed) {
+                layer_on(_ADJUST);
+            } else {
+                layer_off(_ADJUST);
+            }
+            return false;
+            break;
+        case LIFT:
+            if (record->event.pressed) {
+                layer_on(_LIFT);
+            } else {
+                layer_off(_LIFT);
+            }
+            return false;
+            break;
+        case GREEK:
+            if (record->event.pressed) {
+                layer_on(_GREEK);
+            } else {
+                layer_off(_GREEK);
+            }
+            return false;
+            break;
         case ALPHA:
             if (record->event.pressed) {
                 SEND_STRING("\\alpha");
