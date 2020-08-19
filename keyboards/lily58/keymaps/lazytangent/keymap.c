f73df880cfe61049a0639e25ea49dcd2e48e1c77
Lazytangent <pm123633@gmail.com>
2020-08-18 19:30:22 -0500

    Update keymap.c

--- keyboards/lily58/keymaps/lazytangent/keymap.c
+++ keyboards/lily58/keymaps/lazytangent/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT( \
   KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,\
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-  KC_CAPS, _______, _______, _______, KC_LPRN, KC_RPRN,                   KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, _______, _______, \
+  KC_CAPS, _______, _______, KC_ENT,  KC_LPRN, KC_RPRN,                   KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, _______, _______, \
   _______, _______, _______, _______, KC_LCBR, KC_RCBR, _______, _______, _______, _______, KC_LT,   KC_GT,   KC_QUES, _______, \
                              _______, _______, _______, _______, _______, ADJUST,  KC_DEL,  GREEK \
 ),
