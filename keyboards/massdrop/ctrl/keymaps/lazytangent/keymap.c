41c2d593c36767beb52fd573016ca598898b39e5
Peter <pm123633@gmail.com>
2020-08-24 13:56:51 -0500

    ctrl backlight

--- keyboards/massdrop/ctrl/keymaps/lazytangent/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/lazytangent/keymap.c
@@ -72,7 +72,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
-    rgblight_sethsv_noeeprom_teal();
 };
 
 // Runs constantly in the background, in a loop.
