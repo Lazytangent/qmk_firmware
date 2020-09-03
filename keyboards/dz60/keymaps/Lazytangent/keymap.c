9f6e2f00b8edde2f6dafb3ecc1a15105176c4775
Lazytangent <pm123633@gmail.com>
2020-09-03 09:25:53 -0500

    Update keymap.c

--- keyboards/dz60/keymaps/Lazytangent/keymap.c
+++ keyboards/dz60/keymaps/Lazytangent/keymap.c
@@ -55,20 +55,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------------------------------------+
    * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |    \   |
    * |-----------------------------------------------------------------------------------------+
-   * | Caps    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
+   * | Control |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
    * |-----------------------------------------------------------------------------------------+
    * |MO(2)|Shift|  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | RSh |  U  | Del |
    * |-----------------------------------------------------------------------------------------+
-   * | Ctl | Alt | Gui |   MO(1)   | Space |    Backspace    | MO(3) | MO(4) |  L  |  D  |  R  |
+   * | Caps| Alt | Gui |   MO(1)   | Space |    Backspace    | MO(3) | MO(4) |  L  |  D  |  R  |
    * `-----------------------------------------------------------------------------------------'
    */
 
     [_QWERTY] = LAYOUT_Lazy1(
       KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
       KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-      KC_CAPS, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+      KC_LCTL, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
       MO(_2),  KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_DEL,
-      KC_LCTL, KC_LALT, KC_LGUI,         MO(_1),         KC_SPC,        KC_BSPC,          MO(_3),  MO(_4),  KC_LEFT, KC_DOWN, KC_RIGHT
+      KC_CAPS, KC_LALT, KC_LGUI,         MO(_1),         KC_SPC,        KC_BSPC,          MO(_3),  MO(_4),  KC_LEFT, KC_DOWN, KC_RIGHT
     ),
 
   /* DVORAK
@@ -77,20 +77,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |-----------------------------------------------------------------------------------------+
   * | Tab    |  '  |  ,  |  .  |  P  |  Y  |  F  |  G  |  C  |  R  |  L  |  /  |  =  |    \   |
   * |-----------------------------------------------------------------------------------------+
-  * | Caps    |  A  |  O  |  E  |  U  |  I  |  D  |  H  |  T  |  N  |  S  |  -  |    Enter    |
+  * | Control |  A  |  O  |  E  |  U  |  I  |  D  |  H  |  T  |  N  |  S  |  -  |    Enter    |
   * |-----------------------------------------------------------------------------------------+
   * |MO(2)|Shift|  ;  |  Q  |  J  |  K  |  X  |  B  |  M  |  W  |  V  |  Z  | RSh |  U  | Del |
   * |-----------------------------------------------------------------------------------------+
-  * | Ctl | Alt | Gui |   MO(1)   | Space |    Backspace    | MO(3) | MO(4) |  L  |  D  |  R  |
+  * | Caps| Alt | Gui |   MO(1)   | Space |    Backspace    | MO(3) | MO(4) |  L  |  D  |  R  |
   * `-----------------------------------------------------------------------------------------'
   */
 
   [_DVORAK] = LAYOUT_Lazy1(
     KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC,          KC_BSPC,
     KC_TAB,  KC_QUOT, KC_COMM, KC_DOT, KC_P,   KC_Y,   KC_F,   KC_G,  KC_C,    KC_R,    KC_L,    KC_SLSH, KC_EQL,  KC_BSLS,
-    KC_CAPS, KC_A,    KC_O,    KC_E,   KC_U,   KC_I,   KC_D,   KC_H,  KC_T,    KC_N,    KC_S,    KC_MINS, KC_ENT,
+    KC_LCTL, KC_A,    KC_O,    KC_E,   KC_U,   KC_I,   KC_D,   KC_H,  KC_T,    KC_N,    KC_S,    KC_MINS, KC_ENT,
     MO(_2),  KC_LSFT, KC_SCLN, KC_Q,   KC_J,   KC_K,   KC_X,   KC_B,  KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT, KC_UP,   KC_DEL,
-    KC_LCTL, KC_LALT, KC_LGUI,         MO(_1),         KC_SPC,        KC_BSPC,          MO(_3),  MO(_4),  KC_LEFT, KC_DOWN, KC_RIGHT
+    KC_CAPS, KC_LALT, KC_LGUI,         MO(_1),         KC_SPC,        KC_BSPC,          MO(_3),  MO(_4),  KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
   /* COLEMAK
@@ -99,20 +99,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |-----------------------------------------------------------------------------------------+
   * | Tab    |  Q  |  W  |  F  |  P  |  G  |  J  |  L  |  U  |  Y  |  ;  |  [  |  ]  |    \   |
   * |-----------------------------------------------------------------------------------------+
-  * | Caps    |  A  |  R  |  S  |  T  |  D  |  H  |  N  |  E  |  I  |  O  |  '  |    Enter    |
+  * | Control |  A  |  R  |  S  |  T  |  D  |  H  |  N  |  E  |  I  |  O  |  '  |    Enter    |
   * |-----------------------------------------------------------------------------------------+
   * |MO(2)|Shift|  Z  |  X  |  C  |  V  |  B  |  K  |  M  |  ,  |  .  |  /  | RSh |  U  | Del |
   * |-----------------------------------------------------------------------------------------+
-  * | Ctl | Alt | Gui |   MO(1)   | Space |    Backspace    | MO(3) | MO(4) |  L  |  D  |  R  |
+  * | Caps| Alt | Gui |   MO(1)   | Space |    Backspace    | MO(3) | MO(4) |  L  |  D  |  R  |
   * `-----------------------------------------------------------------------------------------'
   */
 
   [_COLEMAK] = LAYOUT_Lazy1(
     KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_F,   KC_P,   KC_G,   KC_J,   KC_L,  KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
-    KC_CAPS, KC_A,    KC_R,    KC_S,   KC_T,   KC_D,   KC_H,   KC_N,  KC_E,    KC_I,    KC_O,    KC_QUOT, KC_ENT,
+    KC_LCTL, KC_A,    KC_R,    KC_S,   KC_T,   KC_D,   KC_H,   KC_N,  KC_E,    KC_I,    KC_O,    KC_QUOT, KC_ENT,
     MO(_2),  KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_K,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_DEL,
-    KC_LCTL, KC_LALT, KC_LGUI,         MO(_1),         KC_SPC,        KC_BSPC,          MO(_3),  MO(_4),  KC_LEFT, KC_DOWN, KC_RIGHT
+    KC_CAPS, KC_LALT, KC_LGUI,         MO(_1),         KC_SPC,        KC_BSPC,          MO(_3),  MO(_4),  KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
   /* 1
@@ -121,20 +121,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------------------------------------+
    * |        |     |     |     |     |     |     |     |     |     |     |      |      |      |
    * |-----------------------------------------------------------------------------------------+
-   * |         |     |     |     |  (  |  )  |  _  |  -  |  =  |  +  |     |     |             |
+   * |         |MO(5)|     |     |  (  |  )  |  _  |  -  |  =  |  +  |     |     |             |
    * |-----------------------------------------------------------------------------------------+
    * |     |     |     |     |     |  {  |  }  |  [  |  ]  |  <  |  >  |  ?  | Ins | PgUp|     |
    * |-----------------------------------------------------------------------------------------+
-   * |      |       |       |       |  MO(3) |     Shift        |      |     | Home| PgDn| End |
+   * |      |       |       |       |        |     Shift        |      |     | Home| PgDn| End |
    * `-----------------------------------------------------------------------------------------'
    */
 
     [_1] = LAYOUT_Lazy1(
       KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS,           _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, KC_LPRN, KC_RPRN, KC_UNDS, KC_MINS, KC_EQL,   KC_PLUS, KC_COLN, KC_DQUO, _______,
+      _______, MO(_3),  _______, _______, KC_LPRN, KC_RPRN, KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, KC_COLN, KC_DQUO, _______,
       _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_LT,   KC_GT,   KC_QUES, KC_INS,  KC_PGUP, _______,
-      _______, _______, _______,          _______,          MO(_3),           KC_LSFT,          _______, _______, KC_HOME, KC_PGDN, KC_END
+      _______, _______, _______,          _______,          _______,          KC_LSFT,          _______, _______, KC_HOME, KC_PGDN, KC_END
     ),
 
   /* 2
@@ -163,7 +163,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------------------------------------------------------.
    * |  ~  | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |   Bspc    |
    * |-----------------------------------------------------------------------------------------+
-   * |        | QWRT| DVRK| CLMK|CtrlT|     |     |     |     |     |      |      |      |     |
+   * |        | QWRT| DVRK| CLMK|CtrlT|     |     | Home| PgDn| PgUp| End |      |      |      |
    * |-----------------------------------------------------------------------------------------+
    * |         |     |     |     |     |     |     | Left| Down| Up  |Right|     |             |
    * |-----------------------------------------------------------------------------------------+
