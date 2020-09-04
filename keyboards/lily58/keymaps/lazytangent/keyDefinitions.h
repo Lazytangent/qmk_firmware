56a9e3a5095c60c4c6d36ed5f8fee23b0825b7b4
Lazytangent <pm123633@gmail.com>
2020-09-04 15:51:11 -0500

    lily58 number rows and FLAYER

--- keyboards/lily58/keymaps/lazytangent/keyDefinitions.h
+++ keyboards/lily58/keymaps/lazytangent/keyDefinitions.h
@@ -3,41 +3,17 @@
 #define _RAISE 2
 #define _ADJUST 3
 #define _LIFT 4
-#define _GREEK 5
+#define _FLAYER 5
 #define LO_LSFT LM(0, MOD_LSFT)
 #define RA_LSFT LM(0, MOD_LSFT)
 
 enum custom_keycodes {
-    ALPHA = SAFE_RANGE,
-    BETA,
-    GAMMA,
-    DELTA,
-    EPSILON,
-    ZETA,
-    ETA,
-    THETA,
-    IOTA,
-    KAPPA,
-    LAMBDA,
-    MU,
-    NU,
-    XI,
-    OMNICRON,
-    PI,
-    RHO,
-    SIGMA,
-    TAU,
-    UPSILON,
-    PHI,
-    CHI,
-    PSI,
-    OMEGA,
-    QWERTY,
+    QWERTY = SAFE_RANGE,
     LOWER,
     RAISE,
     ADJUST,
     LIFT,
-    GREEK
+    FLAYER
 };
 
 void set_keylog(uint16_t keycode, keyrecord_t *record);
@@ -93,133 +69,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
             break;
-        case GREEK:
+        case FLAYER:
             if (record->event.pressed) {
-                layer_on(_GREEK);
+                layer_on(_FLAYER);
             } else {
-                layer_off(_GREEK);
+                layer_off(_FLAYER);
             }
             return false;
-            break;
-        case ALPHA:
-            if (record->event.pressed) {
-                SEND_STRING("\\alpha");
-            }
-            break;
-        case BETA:
-            if (record->event.pressed) {
-                SEND_STRING("\\beta");
-            }
-            break;
-        case GAMMA:
-            if (record->event.pressed) {
-                SEND_STRING("\\gamma");
-            }
-            break;
-        case DELTA:
-            if (record->event.pressed) {
-                SEND_STRING("\\delta");
-            }
-            break;
-        case EPSILON:
-            if (record->event.pressed) {
-                SEND_STRING("\\epsilon");
-            }
-            break;
-        case ZETA:
-            if (record->event.pressed) {
-                SEND_STRING("\\zeta");
-            }
-            break;
-        case ETA:
-            if (record->event.pressed) {
-                SEND_STRING("\\eta");
-            }
-            break;
-        case THETA:
-            if (record->event.pressed) {
-                SEND_STRING("\\theta");
-            }
-            break;
-        case IOTA:
-            if (record->event.pressed) {
-                SEND_STRING("\\iota");
-            }
-            break;
-        case KAPPA:
-            if (record->event.pressed) {
-                SEND_STRING("\\kappa");
-            }
-            break;
-        case LAMBDA:
-            if (record->event.pressed) {
-                SEND_STRING("\\lambda");
-            }
-            break;
-        case MU:
-            if (record->event.pressed) {
-                SEND_STRING("\\mu");
-            }
-            break;
-        case NU:
-            if (record->event.pressed) {
-                SEND_STRING("\\nu");
-            }
-            break;
-        case XI:
-            if (record->event.pressed) {
-                SEND_STRING("\\xi");
-            }
-            break;
-        case OMNICRON:
-            if (record->event.pressed) {
-                SEND_STRING("\\omnicron");
-            }
-            break;
-        case PI:
-            if (record->event.pressed) {
-                SEND_STRING("\\pi");
-            }
-            break;
-        case RHO:
-            if (record->event.pressed) {
-                SEND_STRING("\\rho");
-            }
-            break;
-        case SIGMA:
-            if (record->event.pressed) {
-                SEND_STRING("\\sigma");
-            }
-            break;
-        case TAU:
-            if (record->event.pressed) {
-                SEND_STRING("\\tau");
-            }
-            break;
-        case UPSILON:
-            if (record->event.pressed) {
-                SEND_STRING("\\upsilon");
-            }
-            break;
-        case PHI:
-            if (record->event.pressed) {
-                SEND_STRING("\\phi");
-            }
-            break;
-        case CHI:
-            if (record->event.pressed) {
-                SEND_STRING("\\chi");
-            }
-            break;
-        case PSI:
-            if (record->event.pressed) {
-                SEND_STRING("\\psi");
-            }
-            break;
-        case OMEGA:
-            if (record->event.pressed) {
-                SEND_STRING("\\omega");
-            }
             break;
     }
     return true;
