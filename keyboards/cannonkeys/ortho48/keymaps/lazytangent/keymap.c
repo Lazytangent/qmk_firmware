9f4fa8c95bfc93fe9886a9ba62e9644eed2b839d
Peter <pm123633@gmail.com>
2021-01-10 16:44:23 -0600

    Ortho48

--- keyboards/cannonkeys/ortho48/keymaps/lazytangent/keymap.c
+++ keyboards/cannonkeys/ortho48/keymaps/lazytangent/keymap.c
@@ -17,12 +17,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 #define _QWERTY 0
-#define _DVORAK 1
-#define _COLEMAK 2
-#define _LOWER 3
-#define _RAISE 4
-#define _NUM 5
-#define _CHEESE 6
+#define _LOWER 1
+#define _RAISE 2
+#define _NUM 3
+#define _CHEESE 4
 
 /* enum layers {                Unable to use enum for layers on cannonkeys/ortho48 because of size restrictions
   _BASE = SAFE_RANGE,1
@@ -49,50 +47,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, KC_LALT, KC_LGUI, MO(5),   MO(3), KC_SPC, KC_BSPC, MO(4), KC_BSPC, KC_DEL,  KC_UP,   KC_ENT
-),
-
-/* DVORAK
- * ,-----------------------------------------------------------------------------------.
- * | Esc  |   '  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Bspc |   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |  -   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Num  | Ctrl | Alt  | GUI  |Lower |Space | Bksp |Raise | Left | Down |  Up  |Right |
- * `-----------------------------------------------------------------------------------'
- */
-[_DVORAK] = LAYOUT_ortho_4x12(
-    KC_ESC,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,  KC_Y,   KC_F,    KC_G,  KC_C,    KC_R,    KC_L,  KC_BSPC,
-    KC_TAB,  KC_A,    KC_O,    KC_E,    KC_U,  KC_I,   KC_D,    KC_H,  KC_T,    KC_N,    KC_S,  KC_MINS,
-    KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,  KC_X,   KC_B,    KC_M,  KC_W,    KC_V,    KC_Z,  KC_ENT,
-    MO(5),   KC_LCTL, KC_LALT, KC_LGUI, MO(3), KC_SPC, KC_BSPC, MO(4), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
-),
-
-/* COLEMAK
- * ,-----------------------------------------------------------------------------------.
- * | Esc  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Bspc |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |  '   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Num  | Ctrl | Alt  | GUI  |Lower |Space | Bksp |Raise | Left | Down |  Up  |Right |
- * `-----------------------------------------------------------------------------------'
- */
-[_COLEMAK] = LAYOUT_ortho_4x12(
-    KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,  KC_G,   KC_J,    KC_L,  KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-    KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,  KC_D,   KC_H,    KC_N,  KC_E,    KC_I,    KC_O,    KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_K,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    MO(5),   KC_LCTL, KC_LALT, KC_LGUI, MO(3), KC_SPC, KC_BSPC, MO(4), KC_BSPC, KC_DEL,  KC_UP,   KC_ENT
+    KC_LCTL, KC_LALT, KC_LGUI, MO(3),   MO(1), KC_SPC, KC_ENT,  MO(2), KC_BSPC, KC_DEL,  KC_UP,   KC_ENT
 ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Tab  |      |      |      |  (   |   )  |   _  |   -  |   =  |   +  |   \  |  |   |
+ * | Tab  |  `   |   ~  |      |  (   |   )  |   _  |   -  |   =  |   +  |   \  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |  {   |   }  |  [   |   ]  |   <  |   >  |   ?  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -101,9 +63,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_ortho_4x12(
     KC_GRV,  KC_1,    KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-    KC_TAB,  _______, _______,  _______, KC_LPRN, KC_RPRN, KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, KC_BSLS, KC_PIPE,
+    KC_TAB,  KC_GRV,  KC_TILD,  _______, KC_LPRN, KC_RPRN, KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, KC_BSLS, KC_PIPE,
     _______, _______, _______,  _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_LT,   KC_GT,   KC_QUES, KC_ENT,
-    _______, _______, A(KC_F4), G(KC_D), _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    _______, _______, A(KC_F4), G(KC_D), _______, _______, _______, MO(4), KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
 /* Raise
@@ -119,9 +81,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_ortho_4x12(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+    KC_DEL,  KC_GRV,  KC_TILD, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
     _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_ENT,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BRID, KC_BRIU, MO(_CHEESE)
+    _______, _______, _______, _______, MO(4), _______, _______, _______, _______, KC_BRID, KC_BRIU, MO(_CHEESE)
 ),
 
 /* Num -- Numpad Layer
@@ -136,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_NUM] = LAYOUT_ortho_4x12(
-    _______, DF(0),   DF(1),   DF(2),   _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, KC_PMNS, KC_BSPC,
+    _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, KC_PMNS, KC_BSPC,
     _______, _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PAST, KC_PPLS, _______,
     _______, _______, _______, _______, _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PDOT, KC_PEQL, KC_PENT,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, LCA(KC_DOWN), LCA(KC_UP), _______
