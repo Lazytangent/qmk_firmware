4f8fccad36a105dadf8c6f1bbd383a506246004c
Lazytangent <pm123633@gmail.com>
2021-05-03 22:30:57 -0500

    More updates

--- keyboards/kbdfans/maja/keymaps/lazytangent/keymap.c
+++ keyboards/kbdfans/maja/keymaps/lazytangent/keymap.c
@@ -12,21 +12,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT( /* FN */
     KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, KC_BSPC, KC_HOME,
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LCBR, KC_RCBR, KC_PIPE, KC_PGUP,
-    _______, KC_GRV,  KC_TILD, _______, KC_LPRN, KC_RPRN, KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, KC_COLN, KC_DQUO,          _______, KC_PGDN,
-    _______, _______, _______, _______, KC_LCBR, KC_RCBR, _______, KC_LBRC, KC_RBRC, KC_LT,   KC_GT,   KC_QUES, KC_VOLU, KC_MUTE,
-    _______,          _______, _______,          _______,          MO(3),            _______,                   KC_MPRV, KC_VOLD, KC_MNXT
+    _______, KC_GRV,  KC_TILD, KC_BSLS, KC_LPRN, KC_RPRN, KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, KC_COLN, KC_DQUO,          _______, KC_PGDN,
+    _______, _______, _______, KC_PIPE, KC_LCBR, KC_RCBR, _______, KC_LBRC, KC_RBRC, KC_LT,   KC_GT,   KC_QUES, _______, KC_VOLU,
+    _______,          _______, _______,          _______,          MO(3),            KC_DEL,                    KC_MPRV, KC_VOLD, KC_MNXT
   ),
   [2] = LAYOUT( /* FN2 */
     KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, KC_BSPC, KC_HOME,
     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, KC_BSPC, KC_HOME,
-    _______, KC_GRV,  KC_TILD, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,          _______, KC_PGDN,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_VOLU, KC_MUTE,
-    _______,          _______, MO(3),            _______,          _______,          _______,                   KC_MPRV, KC_VOLD, KC_MNXT
+    _______, KC_GRV,  KC_TILD, KC_PIPE, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,          _______, KC_PGDN,
+    _______, _______, _______, _______, _______, _______, MO(3),   _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
+    _______,          _______, MO(3),            _______,          _______,          _______,                   _______, _______, _______
   ),
   [3] = LAYOUT( /* FN3 */
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  RESET,   KC_HOME,
     _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, KC_PSCR, KC_SLCK, KC_PAUS, RESET,   KC_PGUP,
-    _______, RGB_SPI, RGB_SPD, KC_TRNS, _______, _______, _______, _______, _______, _______, _______, _______,          EEP_RST, KC_PGDN,
+    _______, RGB_SPI, RGB_SPD, _______, _______, _______, _______, _______, _______, _______, _______, _______,          EEP_RST, KC_PGDN,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______,          _______, _______,          _______,          _______,          _______,                   _______, _______, _______
   ),
