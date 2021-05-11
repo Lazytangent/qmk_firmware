8b17b0ec73da95fd8860c9795720611fec7a4c5c
Lazytangent <pm123633@gmail.com>
2021-05-10 21:40:09 -0500

    Update cannonkeys ortho48

--- keyboards/cannonkeys/ortho48/keymaps/lazytangent/keymap.c
+++ keyboards/cannonkeys/ortho48/keymaps/lazytangent/keymap.c
@@ -44,10 +44,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSLS,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_ESC,  KC_LALT, KC_LGUI, MO(3),   MO(1), KC_SPC, KC_ENT,  MO(2), KC_BSPC, KC_DEL,  KC_UP,   KC_ENT
+    KC_ESC,  KC_LALT, KC_LGUI, MO(3),   MO(1), KC_SPC, KC_BSPC, MO(2), KC_BSPC, KC_DEL,  KC_RALT, KC_ENT
 ),
 
 /* Lower
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_1,    KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
     KC_TAB,  KC_GRV,  KC_TILD,  _______, KC_LPRN, KC_RPRN, KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, KC_BSLS, KC_PIPE,
     _______, _______, _______,  _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_LT,   KC_GT,   KC_QUES, KC_ENT,
-    _______, _______, A(KC_F4), G(KC_D), _______, _______, _______, MO(4), KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    _______, _______, A(KC_F4), G(KC_D), _______, _______, _______, MO(4),   KC_DEL,  KC_PGDN, KC_PGUP, KC_END
 ),
 
 /* Raise
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
     KC_DEL,  KC_GRV,  KC_TILD, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
     _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_ENT,
-    _______, _______, _______, _______, MO(4), _______, _______, _______, _______, KC_BRID, KC_BRIU, MO(_CHEESE)
+    _______, _______, _______, _______, MO(4),   _______, _______, _______, _______, KC_BRID, KC_BRIU, MO(_CHEESE)
 ),
 
 /* Num -- Numpad Layer
