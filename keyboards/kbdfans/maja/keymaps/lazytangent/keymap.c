d1dcb995043138d1f56cd1519cd924060317590e
Lazytangent <pm123633@gmail.com>
2021-05-28 14:41:56 -0500

    Update maja keymap

--- keyboards/kbdfans/maja/keymaps/lazytangent/keymap.c
+++ keyboards/kbdfans/maja/keymaps/lazytangent/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [2] = LAYOUT( /* FN2 */
     KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, KC_BSPC, KC_HOME,
     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, KC_BSPC, KC_HOME,
-    _______, KC_GRV,  KC_TILD, KC_PIPE, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,          _______, KC_PGDN,
+    _______, KC_GRV,  KC_TILD, KC_PIPE, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______, KC_PGDN,
     _______, _______, _______, _______, _______, _______, MO(3),   _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
     _______,          _______, MO(3),            _______,          _______,          _______,                   _______, _______, _______
   ),
