0c69f39ebf89dcefb3ac2c643e94429af7e6aafa
Lazytangent <pm123633@gmail.com>
2021-06-30 15:10:16 -0500

    Update dz60 for emacs

--- keyboards/dz60/keymaps/lazytangent/keymap.c
+++ keyboards/dz60/keymaps/lazytangent/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
       KC_LCTL,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
       KC_ESC,    KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_DEL,
-      KC_LALT,   KC_LALT, KC_LGUI,         MO(_1),         KC_SPC,        MO(_2),           MO(_3),  MO(_4),  KC_LEFT, KC_DOWN, KC_RIGHT
+      KC_LALT,   KC_LALT, KC_LGUI,         MO(_1),         KC_SPC,        MO(_2),           KC_RALT, MO(_4),  KC_LEFT, KC_DOWN, KC_RIGHT
     ),
 
   /* GAME
@@ -179,7 +179,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_PIPE,
         _______, KC_GRV,  KC_TILD, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, _______,
-        _______, _______, _______,          MO(_2),           _______,          _______,          _______, _______, _______, _______, _______
+        _______, _______, _______,          MO(_3),           _______,          _______,          _______, _______, _______, _______, _______
     ),
 
   /* 3 Windows Shortcuts & i3WM Things
