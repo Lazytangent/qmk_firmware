5d57cbe023980082624faac35c4b1d17182a4186
Peter Mai <pm123633@gmail.com>
2021-08-26 09:21:38 -0500

    Update right thumb cluster

--- keyboards/moonlander/keymaps/lazytangent/keymap.c
+++ keyboards/moonlander/keymaps/lazytangent/keymap.c
@@ -45,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BSLS,           KC_DEL,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,           KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_LALT, KC_ULT,  KC_MEH,  KC_LGUI, MO(1),           G(KC_SPC),          _______,          MO(2),   KC_BSPC,A(KC_LCTL),KC_RGUI,KC_RALT,
-                                            KC_SPC,  MO(1),   KC_LP,             KC_MC,   MO(2),   KC_ENT
+        KC_LALT, KC_ULT,  KC_MEH,  KC_LGUI, MO(1),           G(KC_SPC),          KC_ESC,           MO(2),   KC_BSPC,A(KC_LCTL),KC_RGUI,KC_RALT,
+                                            KC_SPC,  MO(1),   KC_LP,             KC_MC,   KC_ENT,  MO(2)
     ),
 
     [1] = LAYOUT_moonlander(
