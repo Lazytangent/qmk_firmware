37a3ae948b606119792fbd345a77a2a315aa7938
Lazytangent <peter.tn.mai@gmail.com>
2022-10-10 12:17:02 -0500

    Add dvorak layer

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/lazytangent/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/lazytangent/keymap.c
@@ -9,14 +9,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_LBRC, KC_RBRC,                                                        KC_MINS, KC_EQL,
                                                      MO(1),   KC_SPC,   KC_ENT,  MO(2),
                                                      KC_TAB,  KC_ESC,   KC_BSPC, KC_DEL,
-                                                     KC_LGUI, KC_LALT,  KC_LGUI, KC_LALT
+                                                     KC_LGUI, KC_LALT,  KC_RALT, KC_RGUI
     ),
 
     [1] = LAYOUT_5x6(
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
         _______, KC_GRV,  KC_TILD, KC_BSLS, KC_LPRN, KC_RPRN,                    KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, KC_COLN, KC_DQUO,
-        _______, _______, _______, KC_PIPE, KC_LCBR, KC_RCBR,                    KC_LBCR, KC_RBRC, KC_LT,   KC_GT,   KC_QUES, _______,
+        TG(3),   _______, _______, KC_PIPE, KC_LCBR, KC_RCBR,                    KC_LBCR, KC_RBRC, KC_LT,   KC_GT,   KC_QUES, _______,
                           _______, _______,                                                        _______, _______,
                                                      _______, _______,  _______, _______,
                                                      _______, _______,  _______, _______,
@@ -26,13 +26,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2] = LAYOUT_5x6(
         KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        _______, _______, _______, _______, _______, KC_LBRC,                    _______, _______, _______, _______, _______, _______,
+        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
         _______, KC_LEFT, KC_UP  , KC_DOWN, KC_RGHT, KC_LPRN,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
         _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
                           _______, _______,                                                        _______, _______,
                                                      _______, _______,  _______, _______,
                                                      _______, _______,  _______, _______,
                                                      _______, _______,  _______, _______
+    ),
+
+    [3] = LAYOUT_5x6(
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                       KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
+        KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                       KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
+        KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,                       KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
+                          KC_LBRC, KC_RBRC,                                                        KC_EQL,  KC_BSLS,
+                                                     MO(1),   KC_SPC,   KC_ENT,  MO(2),
+                                                     KC_TAB,  KC_ESC,   KC_BPSC, KC_DEL,
+                                                     KC_LGUI, KC_LALT,  KC_RALT, KC_RGUI
     )
 };
 
