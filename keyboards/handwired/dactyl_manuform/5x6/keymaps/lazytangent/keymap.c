184d65038c00e3c2ed71f9eaacd8b44970fc3880
Lazytangent <peter.tn.mai@gmail.com>
2022-10-11 21:08:58 -0500

    Update layer 2

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/lazytangent/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/lazytangent/keymap.c
@@ -27,8 +27,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [2] = LAYOUT_5x6(
         KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-        _______, KC_LEFT, KC_UP  , KC_DOWN, KC_RGHT, KC_LPRN,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-        _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
+        _______, KC_GRV,  KC_TILD, KC_BSLS, KC_LPRN, KC_RPRN,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+        _______, _______, _______, KC_PIPE, KC_LCBR, KC_RCBR,                    _______, _______, _______, _______, _______, _______,
                           _______, _______,                                                        _______, _______,
                                                      _______, _______,  _______, _______,
                                                      _______, _______,  _______, _______,
