54ad3e5b28f519115ee4fc01e598af29506e3532
Lazytangent <peter.tn.mai@gmail.com>
2022-10-25 13:54:05 -0500

    Another update to thumb cluster

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/lazytangent/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/lazytangent/keymap.c
@@ -6,9 +6,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                          KC_LALT, KC_LGUI,                                                        KC_MINS, KC_EQL,
+                          KC_LBRC, KC_RBRC,                                                        KC_MINS, KC_EQL,
                                                      KC_LBRC, MO(2),    MO(3),   KC_RBRC,
-                                                     KC_TAB,  KC_SPC,   KC_ENT,  KC_BSPC,
+                                                     KC_LCTL, KC_SPC,   KC_ENT,  KC_BSPC,
                                                      KC_LALT, KC_LGUI,  KC_LCTL, KC_RGUI
     ),
 
@@ -19,16 +19,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,                       KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
                           KC_LBRC, KC_RBRC,                                                        KC_EQL,  KC_BSLS,
                                                      KC_LBRC, MO(2),    MO(3),   KC_RBRC,
-                                                     KC_TAB,  KC_SPC,   KC_ENT,  KC_BSPC,
-                                                     KC_LGUI, KC_LALT,  KC_RALT, KC_RGUI
+                                                     KC_LCTL, KC_SPC,   KC_ENT,  KC_BSPC,
+                                                     KC_LALT, KC_LGUI,  KC_LCTL, KC_RGUI
     ),
 
     [2] = LAYOUT_5x6(
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ESC,
         _______, KC_GRV,  KC_TILD, KC_BSLS, KC_LPRN, KC_RPRN,                    KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, KC_COLN, KC_DQUO,
         TG(1),   _______, _______, KC_PIPE, KC_LCBR, KC_RCBR,                    KC_LCBR, KC_RBRC, KC_LT,   KC_GT,   KC_QUES, _______,
-                          _______, _______,                                                        _______, _______,
+                          _______, MO(4),                                                          KC_BSLS, KC_PIPE,
                                                      _______, _______,  _______, _______,
                                                      _______, _______,  _______, _______,
                                                      _______, _______,  _______, _______
@@ -45,6 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                      _______, KC_LEAD,  _______, _______,
                                                      _______, _______,  _______, _______
     ),
+
 };
 
 /* [n] = LAYOUT_5x6( */
