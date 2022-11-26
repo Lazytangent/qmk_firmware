4347948917697e9d9870ba403cecdcc441148237
Lazytangent <peter.tn.mai@gmail.com>
2022-11-26 15:24:09 -0600

    Add ESC to dactyl keymap

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/lazytangent/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/lazytangent/keymap.c
@@ -6,7 +6,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                          KC_LBRC, KC_RBRC,                                                        KC_MINS, KC_EQL,
+                          KC_LBRC, KC_RBRC,                                                       S(KC_TAB),KC_TAB,
                                                      KC_LBRC, MO(2),    MO(3),   KC_RBRC,
                                                      KC_LCTL, KC_SPC,   KC_ENT,  KC_BSPC,
                                                      KC_LALT, KC_LGUI,  KC_LCTL, KC_RGUI
@@ -26,11 +26,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [2] = LAYOUT_5x6(
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ESC,
-        _______, KC_GRV,  KC_TILD, KC_BSLS, KC_LPRN, KC_RPRN,                    KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, KC_COLN, KC_DQUO,
+        KC_ESC,  KC_GRV,  KC_TILD, KC_BSLS, KC_LPRN, KC_RPRN,                    KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, KC_COLN, KC_DQUO,
         TG(1),   _______, _______, KC_PIPE, KC_LCBR, KC_RCBR,                    KC_LBRC, KC_RBRC, KC_LT,   KC_GT,   KC_QUES, _______,
                           _______, MO(4),                                                          KC_BSLS, KC_PIPE,
                                                      _______, _______,  _______, _______,
-                                                     _______, _______,  _______, _______,
+                                                     _______, _______,  KC_ESC,  KC_DEL,
                                                      _______, _______,  _______, _______
 
     ),
