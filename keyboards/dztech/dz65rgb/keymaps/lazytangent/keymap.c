38dbc7bb278de8f0c4b738223757cf884bb1a379
Lazytangent <peter.tn.mai@gmail.com>
2023-05-16 15:14:08 -0500

    Update dz65rgb map

--- keyboards/dztech/dz65rgb/keymaps/lazytangent/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/lazytangent/keymap.c
@@ -6,7 +6,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, MO(3),
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  TG(1),
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_DEL,
-        KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                             KC_LEAD, KC_RGUI, MO(2),   KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                             KC_RGUI, KC_RALT, MO(2),   KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [1] = LAYOUT_65_ansi(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC, KC_BSPC, KC_ESC,
@@ -16,28 +16,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                    KC_LEAD, KC_RGUI, MO(2),   KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [2] = LAYOUT_65_ansi(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  RESET,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  QK_BOOT,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______,                            _______,                   _______, MO(3),   _______, _______, _______, _______
     ),
     [3] = LAYOUT_65_ansi(
-        KC_ESC,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  _______, RESET,
+        KC_ESC,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  _______, QK_BOOT,
         _______, RGB_HUI, RGB_MOD, RGB_SAI, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, RGB_HUD,RGB_RMOD, RGB_SAD, RGB_VAD, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
         _______,          _______, _______, _______, _______, RGB_M_B, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______,                            _______,                   _______, MO(3),   _______, _______, _______, _______
-    ),
+    )
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-
-    }
-    return true;
-
-}
 
 // Template
 /* _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, */
@@ -45,22 +37,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 /* _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, */
 /* _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, */
 /* _______, _______, _______,                   _______,                            _______, _______, _______, _______, _______, _______  */
-
-LEADER_EXTERNS();
-
-void matrix_scan_user(void) {
-    LEADER_DICTIONARY() {
-        leading = false;
-        leader_end();
-
-        SEQ_ONE_KEY(KC_T) {
-            SEND_STRING(SS_TAP(X_F19));
-        }
-        SEQ_ONE_KEY(KC_R) {
-            SEND_STRING(SS_TAP(X_F18));
-        }
-        SEQ_ONE_KEY(KC_A) {
-            SEND_STRING(SS_TAP(X_F17));
-        }
-    }
-}
