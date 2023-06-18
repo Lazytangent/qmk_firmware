7cf190e770619cf3d0f24422b136ab37318565fd
Lazytangent <peter.tn.mai@gmail.com>
2023-06-17 20:46:00 -0500

    Update keymap

--- keyboards/keychron/q8/ansi/keymaps/lazytangent/keymap.c
+++ keyboards/keychron/q8/ansi/keymaps/lazytangent/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_EXLM,  KC_AT,    KC_HASH, KC_DLR,  KC_PERC, KC_CIRC,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_UNDS,  KC_PLUS,  _______,          QK_BOOT,
         _______, KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     _______,  _______,  _______,          _______,
         _______, KC_GRV,   KC_TILD,  KC_BSLS, KC_LPRN, KC_RPRN,           KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS,  KC_COLN,  KC_DQUO,  _______,          _______,
-        _______,           _______,  _______, KC_PIPE, KC_LCBR, KC_RCBR,  _______, KC_LBRC, KC_RBRC, _______,  _______,  _______,  _______, _______,
+        _______,           _______,  _______, KC_PIPE, KC_LCBR, KC_RCBR,  _______, KC_LBRC, KC_RBRC, KC_LT,    KC_GT,    _______,  _______, _______,
         _______, _______,  _______,           _______,          _______,  MO(4),            _______,           _______,            _______, _______, _______
     ),
 
